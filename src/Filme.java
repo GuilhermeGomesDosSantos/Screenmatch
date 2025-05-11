cecbc79798daaa8bae80b086a7bc5a7fca5d7772
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-11 14:58:00 -0300

    Um modelo para representar filmes

---
+++ src/Filme.java
@@ -0,0 +1,9 @@
+public class Filme {
+    // A classe especifica o conteúdo de um Objeto
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+}
