2214a288298f5680127cd4520d54a96abdccf04d
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-17 10:44:53 -0300

    Aproveitando o modelo para séries

--- src/Principal.java
+++ src/Principal.java
@@ -1,4 +1,5 @@
 import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
@@ -14,5 +15,12 @@ public class Principal {
         meuFilme.avalia(10);
         System.out.println("Total de avaliações" + meuFilme.getTotalDeAvaliacoes());
         System.out.println(meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.setTemporadas(10);
+        lost.setEpisodiosPorTemporadas(10);
+        lost.exibeFichaTecnica();
     }
 }
