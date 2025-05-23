363c38551c781608ce9497f1884a57b338f6874a
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-23 20:26:10 -0300

    Classificando episódios e filmes

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -15,6 +15,6 @@ public class Filme extends Titulo implements Classificavel { // o extends está 
 
     @Override
     public int getClassificacao() {
-        return 0;
+        return (int) pegaMedia() / 2;
     }
 }
