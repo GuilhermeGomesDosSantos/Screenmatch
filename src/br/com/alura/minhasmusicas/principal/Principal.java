a51b86119f8163678ee3fd02da998c17bb916cd6
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-05-24 17:52:55 -0300

    Instanciando músicas e podcasts

---
+++ src/br/com/alura/minhasmusicas/principal/Principal.java
@@ -0,0 +1,32 @@
+package br.com.alura.minhasmusicas.principal;
+
+import br.com.alura.minhasmusicas.modelos.Musica;
+import br.com.alura.minhasmusicas.modelos.Podcast;
+
+public class Principal {
+    public static void main(String[] args) {
+        Musica minhaMusica = new Musica();
+        minhaMusica.setTitulo("Forever");
+        minhaMusica.setCantor("Kiss");
+
+        for (int i = 0; i < 1000; i++){
+            minhaMusica.reproduz();
+        }
+
+        for (int i = 0; i < 50; i++){
+            minhaMusica.curtir();
+        }
+
+        Podcast podcast = new Podcast();
+        podcast.setTitulo("Bolha Dev");
+        podcast.setApresentador("Marcos Mendes");
+
+        for (int i = 0; i < 5000; i++){
+            podcast.reproduz();
+        }
+
+        for (int i = 0; i < 1000; i++){
+            podcast.curtir();
+        }
+    }
+}
