faad9200f4de51e28fd2b7250ea645ee9f6d845a
GuilhermeGomesDosSantos <gomes3654gui@gmail.com>
2025-06-22 15:29:57 -0300

    Titulo

--- src/PraticaArquivo/GravarMensagem/Titulo/Main.java
+++ src/PraticaArquivo/GravarMensagem/Titulo/Main.java
@@ -3,6 +3,9 @@ package PraticaArquivo.GravarMensagem.Titulo;
 Defina uma classe chamada Titulo com os atributos necessários.
 Em seguida, crie um programa que instancia um objeto Titulo,
 serializa esse objeto para JSON usando a biblioteca Gson e imprime o resultado.
+
+Modifique o programa anterior para que o JSON gerado seja formatado de maneira mais elegante.
+Utilize o método setPrettyPrinting para alcançar esse resultado.
 */
 
 import com.google.gson.Gson;
